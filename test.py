1368fef99587c190e39278222bddc6f9139186e3
ashishbhardwaj0506 <ashishbhardwaj0506@gmail,com>
2025-02-15 18:35:00 +0530

    Write code for calulate numbers in string

--- test.py
+++ test.py
@@ -1,4 +1,31 @@
 import unittest
+import re
+
+def add_string(input_string):
+    if not input_string:
+        return 0  # Return 0 for empty input
+
+    # Check for a custom delimiter
+    match = re.match(r"//(.+)\n(.*)", input_string)
+    if match:
+        delimiter, numbers = match.groups()
+    else:
+        delimiter, numbers = ",", input_string  # Default delimiter is ","
+
+    try:
+        negative_num = []
+        num_list = list(map(int, numbers.split(delimiter)))
+        for num in num_list:
+            if num < 0:
+                negative_num.append(num)
+        if negative_num and len(negative_num) == 1:
+            raise Exception("Invalid input: No negative numbers pass.")
+        if negative_num and len(negative_num) > 1:
+            raise Exception("Invalid input: No negative numbers pass {}".format(",".join(map(str, negative_num))))
+        return sum(num_list)
+    except ValueError:
+        raise ValueError("Invalid input: Ensure all values are numbers.")
+
 
 class TestStringCalculator(unittest.TestCase):
     def test_empty_string(self):
