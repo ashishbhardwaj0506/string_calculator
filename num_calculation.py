7e66802688107ff69c7cc6e1e5f5fad14788a9a9
ashishbhardwaj0506 <ashishbhardwaj0506@gmail,com>
2025-08-06 10:55:11 +0530

    Added test for only numbers

--- num_calculation.py
+++ num_calculation.py
@@ -7,3 +7,9 @@ class TestCalculator(unittest.TestCase):
 
     def test_empty_string(self):
         self.assertEqual(calculator(""), 0)
+
+    def test_single_number(self):
+        self.assertEqual(calculator("5"), 5)
+
+    def test_multiple_numbers_default_delimiter(self):
+        self.assertEqual(calculator("1,2,3"), 6)
