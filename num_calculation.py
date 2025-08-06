eeaa94051206c95a862d7cb375bb8a177b799f3c
ashishbhardwaj0506 <ashishbhardwaj0506@gmail,com>
2025-08-06 10:58:15 +0530

    Added test with special character

--- num_calculation.py
+++ num_calculation.py
@@ -13,3 +13,15 @@ class TestCalculator(unittest.TestCase):
 
     def test_multiple_numbers_default_delimiter(self):
         self.assertEqual(calculator("1,2,3"), 6)
+
+    def test_delimiter(self):
+        self.assertEqual(calculator("//;\n1;2;3"), 6)
+
+    def test_special_char(self):
+        self.assertEqual(calculator("//*\n2*3*4"), 9)
+
+    def test_single_number(self):
+        self.assertEqual(calculator("//;\n7"), 7)
+
+    def test_not_present_in_numbers(self):
+        self.assertEqual(calculator("//;\n123"), 123)
